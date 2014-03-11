950090464088530d2986d1eed6aaeed3fd80feaa
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-03-11 10:27:44 -0400

    midway to add change_wline_space

--- program/monte_carlo.f90
+++ program/monte_carlo.f90
@@ -99,18 +99,18 @@ SUBROUTINE markov
         !call remove_interaction_cross                  
       !case(11) 
         !call reconnect                      
-      !case(12) 
-        !call shift_gline_in_space          
-      !case(13)  
-        !call shift_wline_in_space         
+      case(12) 
+        call change_gline_space          
+      case(13)  
+        call change_wline_space         
       !case(14) 
         !call change_Gamma_type     
       !case(15) 
         !call move_measuring_index       
       case(16)  
         call change_gline_time        
-      !case(17)  
-        !call shift_wline_in_space         
+      case(17)  
+        call change_wline_time        
     end select
 
     imc = imc + 1
@@ -135,3 +135,64 @@ SUBROUTINE change_gline_time
     implicit none
     
 end SUBROUTINE
+
+SUBROUTINE change_wline_time
+    implicit none
+    integer :: iGam, iWLn, jGam, dxw, dyw, xw, yw, xwj, ywj, dir
+    double precision :: WGam, WW, Anew, Aold, Pacc, sgn
+
+    !------- step1 : check if worm is present -------------
+    if(IsWormPresent .eqv. .true.)    return
+    !ProbProp(iupdate) = ProbProp(iupdate) + 1
+
+    !------- step2 : propose a new config -----------------
+    iGam = generate_gamma()
+    if
+    dir  = DirecVertex(iGam)
+    iWLn = NeighVertex(3, iGam)
+    jGam = NeighLn(3-dir, iWLn)
+
+    dxw = generate_x();         dyw = generate_y()
+    xw = find_neigh_x(WXVertex(iGam), dxw)
+    yw = find_neigh_y(WYVertex(iGam), dyw)
+
+    xwj = WXVertex(jGam);          ywj = WYVertex(jGam)
+    
+    !------- step3 : configuration check ------------------
+
+    !------- step4 : weight calculation -------------------
+    WGam = weight_vertex(Order, StatusVertex(iGam), diff_x(GXVertex(iGam),xw), diff_y(GYVertex(iGam),yw), &
+      & OmegaLn(NeighVertex(1, iGam)), OmegaLn(NeighVertex(2, iGam)), TypeVertex(iGam))
+    WW = weight_line(StatusLn(iWLn), 2, diff_x(xw,xwj), diff_y(yw,ywj), &
+      & OmegaLn(iWLn), TypeLn(iWLn)) 
+
+
+    Anew = WGam *WW
+    Aold = WeightLn(iWLn)*WeightVertex(iGam)
+
+    call weight_ratio(Pacc, sgn, Anew, Aold)
+
+    !Pacc = Pacc*prob_x(WXVertex(iGam))*prob_y(WYVertex(iGam))/(prob_x(dxw)*prob_y(dyw))
+
+    !------- step5 : accept the update --------------------
+    ProbProp(Order, iupdate) = ProbProp(Order, iupdate) + 1
+    if(rn()<=Pacc) then
+
+      !------ update the diagram info -------------------
+      Phase = Phase *sgn
+
+      !------ update the site of elements --------------
+      WXVertex(iGam) = xw
+      WYVertex(iGam) = yw
+
+      !------ update the weight of elements ------------
+      WeightLn(iWLn) = WW
+      WeightVertex(iGam) = WGam
+
+      call update_weight(Anew, Aold)
+
+      ProbAcc(Order, 13) = ProbAcc(Order, 13) + 1
+    endif
+    return
+end SUBROUTINE
+
