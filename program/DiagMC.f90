2c137d40a55ddd13f48fd5c533b7e87781e5fe92
Yuan Huang <huangy22@gmail.com>
2014-03-14 11:53:32 -0400

    add update change_wline_isdelta
    Former-commit-id: e6af1f7d10de4383036d99e9239ae4376e53dd44

--- program/DiagMC.f90
+++ program/DiagMC.f90
@@ -38,6 +38,7 @@ PROGRAM MAIN
   Pupdate(13)  = 1.d0
   Pupdate(14)  = 1.d0
   Pupdate(15)  = 1.d0
+  Pupdate(18)  = 1.d0
 
   !===============  Test variables ==================================
   TestData(:)=0.d0
