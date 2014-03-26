29c1195e78713522b8274bf1f74c0e6515064a25
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-03-26 11:09:27 -0400

    make the genarate xy more compact.

--- program/DiagMC.f90
+++ program/DiagMC.f90
@@ -370,42 +370,40 @@ SUBROUTINE test_subroutine
     implicit none
     integer :: isamp
     !======== test x,y distribution =========================
-    !integer :: i,x,y,N
-    !double precision :: histx(0:MxLx-1),xweight
-    !double precision :: histy(0:MxLy-1),yweight
-    !call initialize_markov
-    !histx(:)=0.d0
-    !histy(:)=0.d0
-    !N=10000000
-    !xweight=0.0
-    !yweight=0.0
-    !do i=1,N
-      !call generate_x(0,x,xweight)
-      !histx(x)=histx(x)+1
-      !call generate_y(0,x,yweight)
-      !histy(y)=histy(y)+1
-    !enddo
-    !open(11,file="testx.dat")
-    !write(11,*) "X:",Lx,logLx
-    !do x=0,Lx-1
-      !write(11,*) x, histx(x)/N, SpatialWeight(1,x)
-    !enddo
-    !close(11)
-    !open(11,file="testy.dat")
-    !write(11,*) "Y:",Ly,logLy
-    !do y=0,Ly-1
-      !write(11,*) y, histy(y)/N, SpatialWeight(2,y)
-    !enddo
-    !close(11)
+    integer :: i,nr(2),cr(2),dr(2),N,x,y
+    double precision :: hist(2,0:MxLx-1),weight
+    call initialize_markov
+    print *,"Testing..."
+    hist(:,:)=0.d0
+    N=100000
+    weight=0.0
+    cr(:)=0
+    do i=1,N
+      call generate_xy(cr,nr,dr,weight,.true.)
+      hist(1,nr(1))=hist(1,nr(1))+1
+      hist(2,nr(2))=hist(2,nr(2))+1
+    enddo
+    open(11,file="testx.dat")
+    write(11,*) "X:",L(1),logL(1)
+    do x=0,Lx
+      write(11,*) x, hist(1,x)/N, SpatialWeight(1,x)
+    enddo
+    close(11)
+    open(11,file="testy.dat")
+    write(11,*) "Y:",L(2),logL(2)
+    do y=0,Ly
+      write(11,*) y, hist(2,y)/N, SpatialWeight(2,y)
+    enddo
+    close(11)
 
     !========  test drawing subroutine =====================
     !call initialize_markov
     !call print_config
 
     !======== analytic_integration =========================
-    call read_GWGamma
-    call calculate_Gam1
-    call output_Gam1
+    !call read_GWGamma
+    !call calculate_Gam1
+    !call output_Gam1
 
     return
 END SUBROUTINE
