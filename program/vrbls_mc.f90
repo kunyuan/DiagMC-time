0c0de1a31a08e5351a70790204cf2c3ed31b8ad9
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-03-27 11:54:02 -0400

    Minor change
    Former-commit-id: 3430e8c00c1fb64e6417c75e6cebc85ccb661ce4

--- program/vrbls_mc.f90
+++ program/vrbls_mc.f90
@@ -123,9 +123,9 @@ MODULE vrbls_mc
 
   !====================== MC Simulation ==================================
   complex*16 :: GamNorm, GamNormWeight           ! the weight of the normalization diagram
-  complex*16, allocatable :: GamMC(:,:,:,:,:,:,:)      ! the measurement of Gamma in MC
-  double precision, allocatable :: ReGamSqMC(:,:,:,:,:,:,:)      ! the measurement of Gamma in MC
-  double precision, allocatable :: ImGamSqMC(:,:,:,:,:,:,:)      ! the measurement of Gamma in MC
+  complex*16, allocatable :: GamMC(:,:,:,:,:,:)      ! the measurement of Gamma in MC
+  double precision, allocatable :: ReGamSqMC(:,:,:,:,:,:)      ! the measurement of Gamma in MC
+  double precision, allocatable :: ImGamSqMC(:,:,:,:,:,:)      ! the measurement of Gamma in MC
 
   double precision :: GamOrder(0:MxOrder)              ! the configuration number of different orders
   double precision :: GamWormOrder(0:MxOrder)          ! the configuration number in whole section
