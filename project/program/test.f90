831a69f39e0a28d4030248e5c186779fc56d34f3
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-03-29 16:33:57 -0400

    add mylib.f90
    Former-commit-id: cee3ccd0200557a94fd7536fdc587357ddc6fc1c

--- project/program/test.f90
+++ project/program/test.f90
@@ -1,8 +1,18 @@
 include "mylib.f90"
 program main
       use string_basic
+      use logging_module
       implicit none
       integer :: i
+      character(len=20) :: a
+      type(logging) :: log_loop
       i=4
-      write(*,*) str(i)
+      a=str(i)
+      write(*,*) a
+      i=8888888
+      write(*,*) str(i),a
+      !log_loop=logging("test.log","hello")
+      call log_loop%SetFileName("test.log")
+      print *,log_loop%FileName
+      print *,log_loop%Content
 end program
