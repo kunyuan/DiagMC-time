6728cf682c9e57e7062d398d4c7ca66b3f88385d
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-03-29 16:33:57 -0400

    add mylib.f90

--- project/program/mylib.f90
+++ project/program/mylib.f90
@@ -4,8 +4,25 @@ contains
   function str(num) result(outstr)
       implicit none
       integer, intent(in) :: num
-      character(len=100) :: outstr
+      character(len=20) :: outstr
       write(outstr,*) num
   end function
 end module
 
+module logging_module
+  implicit none
+  type, public :: logging
+      character(len=256) :: FileName
+      character(len=1024) :: Content
+      integer :: level
+    contains
+      procedure :: SetFileName => set_filename
+  end type logging
+
+contains
+  subroutine set_filename(this,str)
+      class(logging) :: this
+      character(len=*),intent(in) :: str
+      this%filename=str
+  end subroutine
+end module
