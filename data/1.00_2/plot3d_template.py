7a0eea1e140d14745c9692ccadeaa2f297ab53b3
Yuan Huang <huangy22@gmail.com>
2014-04-10 13:05:44 -0400

    reorganize the data files and add the first template

---
+++ data/1.00_2/plot3d_template.py
@@ -0,0 +1,65 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import read_data
+
+is2d = True
+#is2d = False
+
+Beta = 0.5
+N = 64
+
+tau = np.arange(0, Beta, Beta/N)
+Gamt, dim_name = read_data.read_array("./1.00_quantities.dat",["Gamma"])["Gamma"]
+
+#tau = np.arange(0, Beta, Beta/N)
+#SUMChit, dim_name = read_data.read_array("./1.00_quantities.dat",["SUMChi"])["SUMChi"]
+
+#tau = np.arange(0, Beta, Beta/N)
+#Denomt, dim_name = read_data.read_array("./1.00_quantities.dat",["Denom"])["Denom"]
+
+if is2d==True:
+    fig = plt.figure()
+
+    plt.plot(tau, Gamt.diagonal().real)
+    plt.xlabel(dim_name[0])
+    plt.ylabel("diag{Gamma}")
+
+    #plt.plot(tau, SUMChit.real)
+    #plt.xlabel(dim_name[0])
+    #plt.ylabel("sum{Chi(r)}")
+
+    #plt.plot(tau, Denomt[2,2,:].real)
+    #plt.xlabel(dim_name[2])
+    #plt.ylabel("Denom(0,0)")
+
+    #plt.plot(tau, Sigmat.real)
+    #plt.xlabel(dim_name[0])
+    #plt.ylabel("Sigma")
+
+    plt.savefig("diag_Gamma.pdf")
+    
+else:
+    X = np.arange(0, Beta, Beta/N)
+    Y = np.arange(0, Beta, Beta/N)
+    X, Y = np.meshgrid(X, Y)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    surf = ax.plot_surface(
+        X, Y, Gamt.real, rstride=1, cstride=1, cmap=cm.coolwarm,
+        linewidth=0, antialiased=False)
+    # ax.set_zlim(-1.01, 1.01)
+
+    ax.set_xlabel(dim_name[0])
+    ax.set_ylabel(dim_name[1])
+    ax.set_zlabel("Gamma")
+
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    fig.colorbar(surf, shrink=Beta, aspect=5)
+
+    plt.show()
