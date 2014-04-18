e5a940448809cb06682d87653665e2c8945f3dd9
Yuan Huang <huangy22@gmail.com>
2014-04-18 16:59:14 -0400

    change the code to J1-J2 model and try the first self-consistent loop
    Former-commit-id: fdd6eb6e5baf44e54f7264f2da405839c6c25ccc

--- project/inlist.py
+++ project/inlist.py
@@ -21,7 +21,7 @@ print readfile
 # monte carlo job defintion
 mc_dict={
     "__Execute" : "./gamma3.exe",
-    "__Duplicate" : 4,
+    "__Duplicate" : 0,
     "__IsCluster" : False,
     "__AutoRun" : True,
     "IsLoad" : False,
@@ -38,7 +38,7 @@ TO_DO.append(job.JobMonteCarlo(mc_dict))
 # self consist loop job definition
 sc_dict={
     "__Execute" : ["python", "./run_loop.py"],
-    "__Duplicate" : 0,
+    "__Duplicate" : 1,
     "__IsCluster" : False,
     #"__AutoRun" : True, 
     "__AutoRun" : False, 
