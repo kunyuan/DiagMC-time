67fdb81ee1186f5c9b0b507d69f358c6a824446d
Kun Chen <chenkun@mail.ustc.edu.cn>
2014-04-20 16:34:13 -0400

    now just use ./gamma<inputfile to run a job! make life easier
    Former-commit-id: 87a92bcd8c2fd68aa38c736f7d6ab0823aae39d1

--- project/mytop.sh
+++ project/mytop.sh
@@ -1,2 +1,3 @@
-aa=$(ps axu | grep -E "run_loop|gamma3" | grep -v "sh" | grep -v "grep" | awk '{printf "%s,", $2}')
-top -pid ${aa:0:${#aa}-1}
+aa=$(ps axu | grep -E "run_loop|gamma3" | grep -v "grep" | awk '{printf "%s,", $2}')
+echo $aa
+top -p ${aa:0:${#aa}-1}
