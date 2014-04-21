ee11c7602a5c7f5fd90b2b29f431ef1a29ff3ed1
Yuan Huang <huangy22@gmail.com>
2014-04-21 11:32:30 -0400

    make some 1st compare plot

--- project/tools/plot_diag_Gamma.py
+++ project/tools/plot_diag_Gamma.py
@@ -12,29 +12,29 @@ Beta = 0.90
 N = 64
 
 tau = np.arange(0, Beta, Beta/N)
-#GamInt, dim_name = read_data.read_array("./../0.90_Gam1.dat")["Gamma"]
+GamInt, dim_name = read_data.read_array("./../0.90_Gam1.dat")["Gamma"]
 
-#GamMC, dim_name = read_data.read_array("./../data/bare_0.90_1_quantities.dat")["Gamma1"]
+GamMC, dim_name = read_data.read_array("./../0.90_quantities.dat")["Gamma"]
 #Gamma.append(GamMC)
-Gamma=[]
+#Gamma=[]
 #GamMC, dim_name = read_data.read_array("./../data/bold_0.90_2_Gammas.dat")["Gamma2"]
 #Gamma.append(GamMC)
 #GamMC, dim_name = read_data.read_array("./../data/bold_0.90_3_Gammas.dat")["Gamma3"]
 #Gamma.append(GamMC)
-GamMC, dim_name = read_data.read_array("./../data/bold_0.90_4_Gammas.dat")["Gamma4"]
-Gamma.append(GamMC)
+#GamMC, dim_name = read_data.read_array("./../data/bold_0.90_4_Gammas.dat")["Gamma4"]
+#Gamma.append(GamMC)
 
 
 if is2d is True:
     fig = plt.figure()
-    #plt.plot(tau, GamMC.diagonal().real, 'r', 
-            #tau, GamInt.diagonal().real, 'b')
-    plt.plot(
-            tau, Gamma[0].diagonal().real, 'r',
+    plt.plot(tau, GamMC.diagonal().real, 'r', 
+            tau, GamInt.diagonal().real, 'b')
+    #plt.plot(
+            #tau, Gamma[0].diagonal().real, 'r',
             #tau, Gamma[1].diagonal().real, 'b',
             #tau, Gamma[2].diagonal().real, 'g',
             #tau, Gamma[3].diagonal().real, 'r--',
-            )
+            #)
 
     plt.xlabel(dim_name[0])
     plt.ylabel("diag{Gamma}")
