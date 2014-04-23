fc64306adc3ac900ef2f877bd5b43acdf54d14aa
Yuan Huang <huangy22@gmail.com>
2014-04-23 16:32:25 -0400

    update the plot scripts
    Former-commit-id: 6e63e01944f18003639a1e6d3f06806e671352d5

--- project/tools/plot_diag_Gamma.py
+++ project/tools/plot_diag_Gamma.py
@@ -1,68 +1,48 @@
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import read_data
-
-is2d = True
-#is2d = False
 
 Beta = 0.90
 N = 64
 
 tau = np.arange(0, Beta, Beta/N)
-#GamInt, dim_name = read_data.read_array("./../0.90_Gam1.dat")["Gamma"]
-
-GamMC, dim_name = read_data.read_array("./../0.90_quantities.dat")["Gamma"]
-#Gamma.append(GamMC)
-#Gamma=[]
-#GamMC, dim_name = read_data.read_array("./../data/bold_0.90_2_Gammas.dat")["Gamma2"]
-#Gamma.append(GamMC)
-#GamMC, dim_name = read_data.read_array("./../data/bold_0.90_3_Gammas.dat")["Gamma3"]
-#Gamma.append(GamMC)
-#GamMC, dim_name = read_data.read_array("./../data/bold_0.90_4_Gammas.dat")["Gamma4"]
-#Gamma.append(GamMC)
 
 
-if is2d is True:
-    fig = plt.figure()
-    plt.plot(
-            tau, GamMC.diagonal().real, 'r', 
-            #tau, GamInt.diagonal().real, 'b',
-            )
-    #plt.plot(
-            #tau, Gamma[0].diagonal().real, 'r',
-            #tau, Gamma[1].diagonal().real, 'b',
-            #tau, Gamma[2].diagonal().real, 'g',
-            #tau, Gamma[3].diagonal().real, 'r--',
-            #)
+#Gamma=["Gamma1","Gamma2","Gamma3","Gamma4"]
+Gamma=["Gamma1","Gamma2","Gamma3"]
+#Gamma=["Gamma1","Gamma2"]
+#Gamma=["Gamma1"]
 
-    plt.xlabel(dim_name[0])
-    plt.ylabel("diag{Gamma}")
+quan = read_data.read_array("./../data/bold_0.90_4_quantities.dat", Gamma)
 
-    # plt.savefig("0.90_Gamma.pdf")
-    plt.show()
+#Gamma=["Gamma1"]
+#Quans=[]
+#quan = read_data.read_array("./../data/bold_0.90_1_quantities.dat")["Gamma"]
+#Quans.append(quan)
+#quan = read_data.read_array("./../data/bold_0.90_2_quantities.dat", Gamma)
+#Quans.append(quan)
+#quan = read_data.read_array("./../data/bold_0.90_3_quantities.dat", Gamma)
+#Quans.append(quan)
+#quan = read_data.read_array("./../data/bold_0.90_4_quantities.dat", Gamma)
+#Quans.append(quan)
 
-else:
-    X = np.arange(0, Beta, Beta/N)
-    Y = np.arange(0, Beta, Beta/N)
-    X, Y = np.meshgrid(X, Y)
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+fig = plt.figure()
+ax = plt.subplot(111)
 
-    surf = ax.plot_surface(
-        X, Y, GamMC.real, rstride=1, cstride=1, cmap=cm.coolwarm,
-        linewidth=0, antialiased=False)
-    # ax.set_zlim(-1.01, 1.01)
+for key in Gamma:
+    ax.plot(tau, quan[key][0].diagonal().real, label=key)
 
-    ax.set_xlabel(dim_name[0])
-    ax.set_ylabel(dim_name[1])
-    ax.set_zlabel("Gamma")
+#ax.plot(tau, Quans[0][0].diagonal().real, label="Order"+str(1))
+#for i in range(1,4):
+    #ax.plot(tau, Quans[i]["Gamma1"][0].diagonal().real, label="Order"+str(i+1))
 
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+#GamIn, _=read_data.read_array("./../0.90_Gam1.dat")["Gamma"]
+#ax.plot(tau, GamIn.diagonal().real, label="NumIntGam1")
 
-    fig.colorbar(surf, shrink=Beta, aspect=5)
+ax.legend()
 
-    plt.show()
+plt.xlabel("tau")
+plt.ylabel("diag{Gamma}")
+
+# plt.savefig("0.90_Gamma.pdf")
+plt.show()
--- project/tools/plot_sum_chi.py
+++ project/tools/plot_sum_chi.py
@@ -5,9 +5,6 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 
-is2d = True
-#is2d = False
-
 Beta = 0.90
 N = 64
 
@@ -29,39 +26,19 @@ BoldChi.append(Chi)
 Chi,dim_name=read_data.read_array("../data/bold_0.90_4_quantities.dat")["SUMChi"]
 BoldChi.append(Chi)
 
-if is2d is True:
-    fig = plt.figure()
-    plt.plot(
-            tau, BareChi[0].real, 'r',
-            tau, BoldChi[0].real, 'b',
-            tau, BareChi[1].real, 'r*',
-            tau, BoldChi[1].real, 'b*',
-            tau, BareChi[2].real, 'ro',
-            tau, BoldChi[2].real, 'bo')
-    plt.xlabel(dim_name[0])
-    plt.ylabel("sum{Chi(r)}")
+fig = plt.figure()
+ax = plt.subplot(111)
 
-    plt.show()
+for i in range(len(BareChi)):
+    ax.plot(tau, BareChi[i].real, label="bare"+str(i+2))
 
-else:
-    X = np.arange(0, Beta, Beta/N)
-    Y = np.arange(0, Beta, Beta/N)
-    X, Y = np.meshgrid(X, Y)
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+for i in range(len(BoldChi)):
+    ax.plot(tau, BoldChi[i].real, label="bold"+str(i+2))
 
-    surf = ax.plot_surface(
-        X, Y, GamMC.real, rstride=1, cstride=1, cmap=cm.coolwarm,
-        linewidth=0, antialiased=False)
-    # ax.set_zlim(-1.01, 1.01)
+ax.legend()
 
-    ax.set_xlabel(dim_name[0])
-    ax.set_ylabel(dim_name[1])
-    ax.set_zlabel("Gamma")
+plt.xlabel("tau")
+plt.ylabel("sum{Chi(r)}")
 
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    fig.colorbar(surf, shrink=Beta, aspect=5)
-
-    plt.show()
+# plt.savefig("0.90_Gamma.pdf")
+plt.show()
