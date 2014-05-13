9fbfc1b66d7dddca4b142fdb14cbcbfa24dc27eb
Yuan Huang <huangy22@gmail.com>
2014-05-13 13:58:48 +0800

    add problem_report
    Former-commit-id: edd6082a350560e4b72457d4ca9b2e2707290c6d

--- project/tools/diag_Gamma_plot.py
+++ project/tools/diag_Gamma_plot.py
@@ -8,35 +8,21 @@ N = 64
 
 tau = np.arange(0, Beta, Beta/N)
 
-Gamma=["Gamma1","Gamma2","Gamma3","Gamma4"]
-#Gamma=["Gamma1","Gamma2","Gamma3"]
-#Gamma=["Gamma1","Gamma2","Gamma"]
-#Gamma=["Gamma"]
+#Quans=["Gamma1","Gamma2","Gamma3","Gamma4"]
+#Quans=["Gamma1","Gamma2","Gamma3"]
+#Quans=["Gamma1","Gamma2","Gamma"]
+Quans=["Gamma1","Gamma"]
 
-#quan = read_data.read_array("./../../data/conservation/bare_0.90_4_quantities.dat", Gamma)
-quan = read_data.read_array("./bare_2/0.90_quantities.dat", Gamma)
-#quan = read_data.read_array("../0.90_Gam1.dat", Gamma)
-#quan = read_data.read_array("../0.90_quantities.dat", Gamma)
-
-#Gamma=["Gamma1"]
-#Quans=[]
-#quan = read_data.read_array("./../data/bold_0.90_1_quantities.dat")["Gamma"]
-#Quans.append(quan)
-#quan = read_data.read_array("./../data/bold_0.90_2_quantities.dat", Gamma)
-#Quans.append(quan)
-#quan = read_data.read_array("./../data/bold_0.90_3_quantities.dat", Gamma)
-#Quans.append(quan)
-#quan = read_data.read_array("./../0.90_quantities.dat", Gamma)
-#Quans.append(quan)
-#quan = read_data.read_array("./../data/bold_0.90_4_quantities.dat", Gamma)
-#Quans.append(quan)
+DiagGamma=[]
+DiagGamma.append(read_data.read_array("./0.90_1_bold_quantities.dat", Quans))
+DiagGamma.append(read_data.read_array("./0.90_2_bold_quantities.dat", Quans))
 
 fig = plt.figure()
 ax = plt.subplot(111)
 
-for key in Gamma:
-    #ax.plot(tau, quan[key][0].diagonal().imag, label=key)
-    ax.plot(tau, quan[key][0].diagonal().real, label=key)
+for i in range(len(DiagGamma)):
+    for key in Quans:
+        ax.plot(tau, DiagGamma[i][key][0].diagonal().real, label=key)
 
 ax.legend()
 
