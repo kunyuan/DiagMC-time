22ae44099f8e69ba3a2936075b104ff544dd7d5a
Yuan Huang <huangy22@gmail.com>
2014-06-10 15:54:49 -0400

    analyze the data for bare 0.90 L=4, 8 with Order 1-5

--- project/tools/diag_Gamma_plot.py
+++ project/tools/diag_Gamma_plot.py
@@ -23,12 +23,12 @@ Quan2D=["Gamma1","Gamma2","Gamma3","Gamma4","Gamma5"]
 #Quan2D=["Gamma1"]
 #Quan2D=[]
 
-Quan1D=["GammaDiag1","GammaDiag2","GammaDiag3","GammaDiag4","GammaDiag5"]
+#Quan1D=["GammaDiag1","GammaDiag2","GammaDiag3","GammaDiag4","GammaDiag5"]
 #Quan1D=["GammaDiag1","GammaDiag2","GammaDiag3","GammaDiag4"]
 #Quan1D=["GammaDiag1","GammaDiag2","GammaDiag3"]
 #Quan1D=["GammaDiag1","GammaDiag2"]
 #Quan1D=["GammaDiag1"]
-#Quan1D=[]
+Quan1D=[]
 Quan=Quan2D+Quan1D
 #Quan=["Gamma"]
 #print Quans
@@ -37,7 +37,14 @@ DiagGamma=[]
 #DiagGamma.append(read_data.read_array("./0.50_4_bold_quantities.dat", Quan))
 #DiagGamma.append(read_data.read_array("./0.50_5_bold_quantities.dat", Quan))
 #DiagGamma.append(read_data.read_array("./0.90_4_bare_quantities.dat", Quan))
-DiagGamma.append(read_data.read_array("./0.90_5_bold_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("./0.90_5_bold_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("./L8_0.90_4_bare_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("../L8_0.90_4_bare_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("../L16_0.50_4_bare_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("../L8_0.50_4_bare_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("./L8_0.50_4_bare_quantities.dat", Quan))
+#DiagGamma.append(read_data.read_array("../L8_0.50_3_bare_quantities.dat", Quan))
+DiagGamma.append(read_data.read_array("./L8_0.90_5_quantities.dat", Quan))
 
 
 fig = plt.figure()
@@ -50,11 +57,10 @@ ax = plt.subplot(111)
 
 for i in range(len(DiagGamma)):
     for key in Quan2D:
-        ax.plot(tau, ((DiagGamma[i][key][0].diagonal()+DiagGamma[i][key][0].diagonal()[::-1])/2).real, label=key)
+        ax.plot(tau, DiagGamma[i][key][0].diagonal().real, label=key)
         #ax.plot(tau, DiagGamma[i][key][0].diagonal().imag, label=key)
     for key in Quan1D:
-        ax.plot(tau, ((DiagGamma[i][key][0]+DiagGamma[i][key][0][::-1])/2).real, label=key)
-        #ax.plot(tau, DiagGamma[i][key][0].real, label=key)
+        ax.plot(tau, DiagGamma[i][key][0].real, label=key)
         #ax.plot(tau, DiagGamma[i][key][0].imag, label=key)
 
 ax.legend()
