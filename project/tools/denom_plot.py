2ad44279994305ca56bc0db8e5f0d6b3482f5982
Yuan Huang <huangy22@gmail.com>
2014-06-12 15:25:33 -0400

    add the r-dependence Gamma output

--- project/tools/denom_plot.py
+++ project/tools/denom_plot.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import read_data
 
-L = np.arange(0, 16)
+L = np.arange(0, 4)
 Beta = 0.90
 N = 64
 tau = np.arange(0, Beta, Beta/N)
@@ -11,7 +11,19 @@ tau = np.arange(0, Beta, Beta/N)
 Quans=["Denom"]
 
 Files=[]
-Files.append(read_data.read_array("../0.90_quantities.dat", Quans))
+#Files.append(read_data.read_array("./L8_0.50_3_bare_quantities.dat", Quans))
+Files.append(read_data.read_array("./L4_0.90_1_quantities.dat", Quans))
+Files.append(read_data.read_array("./L4_0.90_2_quantities.dat", Quans))
+Files.append(read_data.read_array("./L4_0.90_3_quantities.dat", Quans))
+Files.append(read_data.read_array("./L4_0.90_4_quantities.dat", Quans))
+Files.append(read_data.read_array("./L4_0.90_5_quantities.dat", Quans))
+
+#Files.append(read_data.read_array("./L8_0.90_1_quantities.dat", Quans))
+#Files.append(read_data.read_array("./L8_0.90_2_quantities.dat", Quans))
+#Files.append(read_data.read_array("./L8_0.90_3_quantities.dat", Quans))
+#Files.append(read_data.read_array("./L8_0.90_4_quantities.dat", Quans))
+#Files.append(read_data.read_array("./L8_0.90_5_quantities.dat", Quans))
+
 #Files.append(read_data.read_array("./0.90_4_bold_quantities.dat", Quans))
 #Files.append(read_data.read_array("./0.90_5_bold_quantities.dat", Quans))
 #Files.append(read_data.read_array("./0.90_4_bare_quantities.dat", Quans))
@@ -25,13 +37,13 @@ ax = plt.subplot(111)
 
 for i in range(len(Files)):
     for key in Quans:
-        for j in range(0, 16):
-            for k in range(0, 16):
-                ax.plot(tau, Files[i][key][0][j][k].real, label=key)
+        ax.plot(tau, Files[i][key][0][2][2].real, marker='o', label="Order "+str(i+1))
 
 ax.legend()
 
-plt.xlabel("tau")
-plt.ylabel("Denom(pi/2, pi/2)")
+plt.xlabel("omega")
+plt.ylabel("Denom(pi, pi)")
 
-plt.show()
+plt.savefig("denominator_L4.pdf")
+#plt.savefig("denominator_L8.pdf")
+#plt.show()
