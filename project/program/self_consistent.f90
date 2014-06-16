7850610bafe3ce82d2f07427708a97a8b2420ead
Yuan Huang <huangy22@gmail.com>
2014-06-16 17:18:07 -0400

    change the self-consistent loop into 1-d array FFT still unfinished

--- project/program/self_consistent.f90
+++ project/program/self_consistent.f90
@@ -36,7 +36,7 @@ SUBROUTINE initialize_W
   implicit none
   integer :: typ, t, dx, dy
 
-  W(:,:,:,:) = (0.d0, 0.d0)
+  W(:,:,:) = (0.d0, 0.d0)
 END SUBROUTINE initialize_W
 
 !!--------------- Initialization of Gamma -----------------------
@@ -44,7 +44,7 @@ SUBROUTINE initialize_Gam
   implicit none
   integer :: ityp, it1, it2
 
-  Gam(:,:,:,:,:) = (0.d0, 0.d0)
+  Gam(:,:,:,:) = (0.d0, 0.d0)
 END SUBROUTINE initialize_Gam
  
 !!------- Initialization of G0 in frequency ----------
@@ -63,15 +63,14 @@ END SUBROUTINE initialize_G0F
 !!------- Initialization of W0 in monmentum and frequency ----------
 SUBROUTINE initialize_W0PF
   implicit none
+  integer :: site
   double precision :: ratio
 
-  W0PF(:,:,:) = (0.d0, 0.d0)
-  ratio = 0.25d0*Jcp*real(MxT)/Beta
-
-  W0PF(1,      0, 0) = dcmplx(ratio, 0.d0)
-  W0PF(L(1)-1, 0, 0) = dcmplx(ratio, 0.d0)
-  W0PF(0,      1, 0) = dcmplx(ratio, 0.d0)
-  W0PF(0, L(2)-1, 0) = dcmplx(ratio, 0.d0)
+  W0PF(:,:) = (0.d0, 0.d0)
+  ratio = real(MxT)/Beta
+  do site = 0, Vol-1
+    W0PF(site, 0) = ratio *weight_W0(1, site)
+  enddo
 
   call transfer_W0_r(1)
   call transfer_W0_t(1)
@@ -83,8 +82,8 @@ SUBROUTINE initialize_Gam0PF
   double precision :: ratio
 
   ratio = (real(MxT)/Beta)**2.d0
-  Gam0PF(:,:,:,:) = (0.d0, 0.d0)
-  Gam0PF(0,0,0,0) = dcmplx(ratio, 0.d0)
+  Gam0PF(:,:,:) = (0.d0, 0.d0)
+  Gam0PF(0,0,0) = ratio*weight_Gam0(1, 0)
 
   call transfer_Gam0_r(1)
   call transfer_Gam0_t(1)
@@ -94,34 +93,30 @@ END SUBROUTINE initialize_Gam0PF
 
 SUBROUTINE calculate_Polar
   implicit none
-  integer :: px, py, p(2), omega
+  integer :: p, omega
   integer :: omegaGin, omegaGout
   complex(kind=8) :: Gin, Gout, Gam1
   double precision :: ratio
 
-  Polar(:,:,:) = (0.d0, 0.d0)
+  Polar(:,:) = (0.d0, 0.d0)
 
   ratio = 2.d0/real(MxT)*(Beta/real(MxT))**4.d0
   do omega = 0, MxT-1
     do omegaGin = 0, MxT-1
-      do py = 0, L(2)-1
-        do px = 0, L(1)-1
-          p = (/px, py/)
-          omegaGout = omegaGin - omega
+      do p = 0, Vol-1
+        omegaGout = omegaGin - omega
 
-          Gin = weight_G(1, omegaGin)
-          if(omegaGout>=0) then
-            Gout = weight_G(1, omegaGout)
-            Gam1 = weight_Gam(5, p, omegaGin, omegaGout)
-          else
-            Gout = -1.d0*weight_G(1, omegaGout+MxT)
-            Gam1 = -1.d0*weight_Gam(5, p, omegaGin, omegaGout+MxT)
-          endif
-          
-          Polar(px, py, omega) = Polar(px, py, omega)+d_times_cd(ratio, cdexp((0.d0, -1.d0) &
-            & *2.d0*omegaGout*Pi/MxT)*Gin*Gout*Gam1)
-          !Polar(px, py, omega) = Polar(px, py, omega)+d_times_cd(ratio, Gin*Gout*Gam1)
-        enddo
+        Gin = weight_G(1, omegaGin)
+        if(omegaGout>=0) then
+          Gout = weight_G(1, omegaGout)
+          Gam1 = weight_Gam(5, p, omegaGin, omegaGout)
+        else
+          Gout = -1.d0*weight_G(1, omegaGout+MxT)
+          Gam1 = -1.d0*weight_Gam(5, p, omegaGin, omegaGout+MxT)
+        endif
+        
+        Polar(p, omega) = Polar(p, omega)+d_times_cd(ratio, cdexp((0.d0, -1.d0) &
+          & *2.d0*omegaGout*Pi/MxT)*Gin*Gout*Gam1)
       enddo
     enddo
   enddo
@@ -131,7 +126,7 @@ END SUBROUTINE calculate_Polar
 
 SUBROUTINE calculate_Sigma
   implicit none
-  integer :: px, py, p(2), omega
+  integer :: p, omega
   integer :: omegaG, omegaW
   complex(kind=8) :: G1, W1, Gam1
   double precision :: ratio
@@ -141,21 +136,18 @@ SUBROUTINE calculate_Sigma
   ratio = -3.d0/(real(L(1))*real(L(2))*real(MxT))*(Beta/real(MxT))**4.d0
   do omega = 0, MxT-1
     do omegaG = 0, MxT-1
-      do py = 0, L(2)-1
-        do px = 0, L(1)-1
-          p = (/px, py/)
-          omegaW = omega-omegaG
+      do p = 0, Vol-1
+        omegaW = omega-omegaG
 
-          G1 = weight_G(1, omegaG)
-          if(omegaW>=0) then
-            W1 = weight_W(1, p, omegaW)
-          else
-            W1 = weight_W(1, p, omegaW+MxT)
-          endif
-          Gam1 = weight_Gam(5, p, omegaG, omega)
-          
-          Sigma(omega) = Sigma(omega)+d_times_cd(ratio, G1*W1*Gam1)
-        enddo
+        G1 = weight_G(1, omegaG)
+        if(omegaW>=0) then
+          W1 = weight_W(1, p, omegaW)
+        else
+          W1 = weight_W(1, p, omegaW+MxT)
+        endif
+        Gam1 = weight_Gam(5, p, omegaG, omega)
+        
+        Sigma(omega) = Sigma(omega)+d_times_cd(ratio, G1*W1*Gam1)
       enddo
     enddo
   enddo
@@ -166,29 +158,27 @@ END SUBROUTINE calculate_Sigma
 !!--------- calculate weight for W matrix ---------
 SUBROUTINE calculate_W
   implicit none
-  integer :: omega, px, py
+  integer :: omega, p
 
   !-------- calculate W = W0/(1-W0*G^2*Gamma) ----------------------------
-  W(:,:,:,:) = (0.d0, 0.d0)
+  W(:,:,:) = (0.d0, 0.d0)
 
   do  omega = 0, MxT-1
-    do py = 0, L(2)-1
-      do px = 0, L(1)-1
-        Denom(px,py,omega) = (1.d0, 0.d0) -W0PF(px,py,omega)*Polar(px,py,omega)
-        W(1,px,py,omega) = W0PF(px,py,omega)/Denom(px,py,omega)
-        if(W(1,px,py,omega)/=W(1,px,py,omega)) then
-          call LogFile%QuickLog("calculate_W NaN appears!")
-          stop
-        endif
-        W(3,px,py,omega) = d_times_cd(-1.d0,W(1,px,py,omega))
-        W(5,px,py,omega) = d_times_cd( 2.d0,W(1,px,py,omega))
-      enddo
+    do p = 0, Vol-1
+      Denom(p,omega) = (1.d0, 0.d0) -W0PF(p,omega)*Polar(p,omega)
+      W(1,p,omega) = W0PF(p,omega)/Denom(p,omega)
+      if(W(1,p,omega)/=W(1,p,omega)) then
+        call LogFile%QuickLog("calculate_W NaN appears!")
+        stop
+      endif
+      W(3,p,omega) = d_times_cd(-1.d0,W(1,p,omega))
+      W(5,p,omega) = d_times_cd( 2.d0,W(1,p,omega))
     enddo
   enddo
 
-  W(2,:,:,:) = W(1,:,:,:)
-  W(4,:,:,:) = W(3,:,:,:)
-  W(6,:,:,:) = W(5,:,:,:)
+  W(2,:,:) = W(1,:,:)
+  W(4,:,:) = W(3,:,:)
+  W(6,:,:) = W(5,:,:)
 
   !!-------------- update the matrix and tail ------------
 END SUBROUTINE calculate_W
@@ -221,7 +211,7 @@ END SUBROUTINE calculate_G
 !!------------- calculate the denominator of the susceptibility ---------
 SUBROUTINE calculate_Denom
   implicit none
-  Denom(:,:,:) = (1.d0, 0.d0) -W0PF(:,:,:)*Polar(:,:,:)
+  Denom(:,:) = (1.d0, 0.d0) -W0PF(:,:)*Polar(:,:)
   return
 END SUBROUTINE calculate_Denom
 
@@ -230,7 +220,7 @@ END SUBROUTINE calculate_Denom
 !!------------- calculate the susceptibility ---------------------------
 SUBROUTINE calculate_Chi
   implicit none
-  integer :: px, py, omega
+  integer :: p, omega
   double precision :: ratio
 
   !!-------- calculate Chi = Pi/(1 - W0 * Pi) ------------------
@@ -238,11 +228,9 @@ SUBROUTINE calculate_Chi
   ratio = -1.d0*(real(MxT)/Beta)**2.d0*0.75d0
 
   do  omega = 0, MxT-1
-    do py = 0, L(2)-1
-      do px = 0, L(1)-1
-        Chi(px,py,omega) = Polar(px,py,omega)/Denom(px,py,omega)
-        Chi(px,py,omega) = d_times_cd(ratio, Chi(px,py,omega))
-      enddo
+    do p = 0, Vol-1
+      Chi(p,omega) = Polar(p,omega)/Denom(p,omega)
+      Chi(p,omega) = d_times_cd(ratio, Chi(p,omega))
     enddo
   enddo
   return
@@ -253,26 +241,19 @@ END SUBROUTINE calculate_Chi
 SUBROUTINE plus_minus_W0(Backforth)
   implicit none
   integer, intent(in) :: Backforth
-  integer :: ityp, px, py, omega
 
   if(Backforth/=-1) then
-    W(1,:,:,:) = W(1,:,:,:) + W0PF(:,:,:)
+    W(1,:,:) = W(1,:,:) + W0PF(:,:)
   else 
-    W(1,:,:,:) = W(1,:,:,:) - W0PF(:,:,:)
+    W(1,:,:) = W(1,:,:) - W0PF(:,:)
   endif
 
-  do  omega = 0, MxT-1
-    do py = 0, L(2)-1
-      do px = 0, L(1)-1
-        W(3,px,py,omega) = d_times_cd(-1.d0,W(1,px,py,omega))
-        W(5,px,py,omega) = d_times_cd( 2.d0,W(1,px,py,omega))
-      enddo
-    enddo
-  enddo
+  W(3,:,:) = -1.d0 * W(1,:,:)
+  W(5,:,:) =  2.d0 * W(1,:,:)
 
-  W(2,:,:,:) = W(1,:,:,:)
-  W(4,:,:,:) = W(3,:,:,:)
-  W(6,:,:,:) = W(5,:,:,:)
+  W(2,:,:) = W(1,:,:)
+  W(4,:,:) = W(3,:,:)
+  W(6,:,:) = W(5,:,:)
 
   return
 END SUBROUTINE
@@ -283,113 +264,24 @@ SUBROUTINE plus_minus_Gam0(Backforth)
   integer :: ityp, px, py, omega1, omega2
 
   if(Backforth/=-1) then
-    Gam(1,:,:,:,:) = Gam(1,:,:,:,:) + Gam0PF(:,:,:,:)
-    Gam(2,:,:,:,:) = Gam(2,:,:,:,:) + Gam0PF(:,:,:,:)
-    Gam(5,:,:,:,:) = Gam(5,:,:,:,:) + Gam0PF(:,:,:,:)
-    Gam(6,:,:,:,:) = Gam(6,:,:,:,:) + Gam0PF(:,:,:,:)
+    Gam(1,:,:,:) = Gam(1,:,:,:) + Gam0PF(:,:,:)
+    Gam(2,:,:,:) = Gam(2,:,:,:) + Gam0PF(:,:,:)
+    Gam(5,:,:,:) = Gam(5,:,:,:) + Gam0PF(:,:,:)
+    Gam(6,:,:,:) = Gam(6,:,:,:) + Gam0PF(:,:,:)
   else 
-    Gam(1,:,:,:,:) = Gam(1,:,:,:,:) - Gam0PF(:,:,:,:)
-    Gam(2,:,:,:,:) = Gam(2,:,:,:,:) - Gam0PF(:,:,:,:)
-    Gam(5,:,:,:,:) = Gam(5,:,:,:,:) - Gam0PF(:,:,:,:)
-    Gam(6,:,:,:,:) = Gam(6,:,:,:,:) - Gam0PF(:,:,:,:)
+    Gam(1,:,:,:) = Gam(1,:,:,:) - Gam0PF(:,:,:)
+    Gam(2,:,:,:) = Gam(2,:,:,:) - Gam0PF(:,:,:)
+    Gam(5,:,:,:) = Gam(5,:,:,:) - Gam0PF(:,:,:)
+    Gam(6,:,:,:) = Gam(6,:,:,:) - Gam0PF(:,:,:)
   endif
 
   return
-END SUBROUTINE
+END SUBROUTINE plus_minus_Gam0
 
-
-
-
-
-!SUBROUTINE Gam_mc2matrix_mc
-  !implicit none
-  !integer :: iorder, dx, dy, ityp, iloop, it1, it2, typ
-  !complex*16 :: cgam, normal
-  !logical :: flag(MxOrder)
-  !double precision :: totrerr, totierr
-  !double precision :: rgam, igam, rgam2, igam2, rerr, ierr, rpercenterr, ipercenterr
-
-  !call initialize_Gam
-
-  !normal = GamNormWeight*Z_normal/GamNorm
-
-  !call LogFile%QuickLog("(ErrorRatio):"+str(ratioerr))
-
-  !flag(:) = .true.
-
-  !call LogFile%WriteStamp('i')
-
-  !looporder: do iorder = 1, MCOrder
-    !totrerr = 0.d0
-    !totierr = 0.d0
-    !do it2 = 0, MxT-1
-      !do it1 = 0, MxT-1
-        !rgam = real(GamMC(iorder, 1, 0, 0, it1, it2))/Z_normal
-        !rgam2 = ReGamSqMC(iorder, 1, 0, 0, it1, it2)/Z_normal
-
-        !rerr = sqrt(abs(rgam2)-rgam**2.d0)/sqrt(Z_normal-1)
-        !rerr = rerr* ratioerr
-        !totrerr = totrerr + abs(rerr)
-
-        !igam = dimag(GamMC(iorder, 1, 0, 0, it1, it2))/Z_normal
-        !igam2 = ImGamSqMC(iorder, 1, 0, 0, it1, it2)/Z_normal
-
-        !ierr = sqrt(abs(igam2)-igam**2.d0)/sqrt(Z_normal-1)
-        !ierr = ierr* ratioerr
-        !totierr = totierr + abs(ierr)
-      !enddo
-    !enddo
-
-    !rgam = SUM(abs(real(GamMC(iorder, 1,0,0,:,:))))/Z_normal
-    !if(rgam<1.d-30) then
-      !rpercenterr = 0.d0
-    !else
-      !rpercenterr = totrerr/rgam
-    !endif
-
-    !if(rpercenterr>MxError) then
-      !flag(iorder)=.false.
-    !endif
-
-    !igam = SUM(abs(dimag(GamMC(iorder, 1,0,0,:,:))))/Z_normal
-    !if(igam<1.d-30) then
-      !ipercenterr = 0.d0
-    !else
-      !ipercenterr = totierr/igam
-    !endif
-
-    !if(ipercenterr>MxError) then
-      !flag(iorder)=.false.
-    !endif
-
-    !call LogFile%WriteLine("Order "+str(iorder)+" relative error: "+str(rpercenterr,'(f6.3)')+","+str(ipercenterr,'(f6.3)')+", accept: "+str(flag(iorder)))
-
-    !if(flag(iorder)) then
-      !do it2 = 0, MxT-1
-        !do it1 = 0, MxT-1
-          !do dx = 0, L(1)-1
-            !do dy = 0, L(2)-1
-              !do ityp = 1, NTypeGam/2
-                !cgam = GamMC(iorder,ityp,dx,dy,it1,it2)/Z_normal
-                !typ = 2*(ityp-1)+1
-                !Gam(typ,dx,dy,it1,it2) = Gam(typ,dx,dy,it1,it2)+ cgam*normal
-              !enddo
-            !enddo
-          !enddo
-        !enddo
-      !enddo
-    !endif
-
-  !enddo looporder
-  !Gam(2,:,:,:,:) = Gam(1,:,:,:,:)
-  !Gam(4,:,:,:,:) = Gam(3,:,:,:,:)
-  !Gam(6,:,:,:,:) = Gam(5,:,:,:,:)
-  
-!END SUBROUTINE Gam_mc2matrix_mc
  
 SUBROUTINE Gam_mc2matrix_mc
   implicit none
-  integer :: iorder, dx, dy, ityp, iloop, it1, it2, typ
+  integer :: iorder, dr, ityp, iloop, it1, it2, typ
   integer :: ibin, ibasis
   complex*16 :: cgam, normal
   logical :: flag(MxOrder)
@@ -410,22 +302,22 @@ SUBROUTINE Gam_mc2matrix_mc
     totrerr = 0.d0
     totierr = 0.d0
     do it1 = 0, MxT-1
-      rgam = real(GamMC(iorder, 0, 0, it1))/Z_normal
-      rgam2 = ReGamSqMC(iorder, 0, 0, it1)/Z_normal
+      rgam = real(GamMC(iorder, 0, it1))/Z_normal
+      rgam2 = ReGamSqMC(iorder, 0, it1)/Z_normal
 
       rerr = sqrt(abs(rgam2)-rgam**2.d0)/sqrt(Z_normal-1)
       rerr = rerr* ratioerr
       totrerr = totrerr + abs(rerr)
 
-      igam = dimag(GamMC(iorder, 0, 0, it1))/Z_normal
-      igam2 = ImGamSqMC(iorder, 0, 0, it1)/Z_normal
+      igam = dimag(GamMC(iorder, 0, it1))/Z_normal
+      igam2 = ImGamSqMC(iorder, 0, it1)/Z_normal
 
       ierr = sqrt(abs(igam2)-igam**2.d0)/sqrt(Z_normal-1)
       ierr = ierr* ratioerr
       totierr = totierr + abs(ierr)
     enddo
 
-    rgam = SUM(abs(real(GamMC(iorder, 0,0,:))))/Z_normal
+    rgam = SUM(abs(real(GamMC(iorder, 0, :))))/Z_normal
     if(rgam<1.d-30) then
       rpercenterr = 0.d0
     else
@@ -436,7 +328,7 @@ SUBROUTINE Gam_mc2matrix_mc
       flag(iorder)=.false.
     endif
 
-    igam = SUM(abs(dimag(GamMC(iorder, 0,0,:))))/Z_normal
+    igam = SUM(abs(dimag(GamMC(iorder, 0, :))))/Z_normal
     if(igam<1.d-30) then
       ipercenterr = 0.d0
     else
@@ -451,31 +343,29 @@ SUBROUTINE Gam_mc2matrix_mc
 
 
     if(flag(iorder)) then
-      do it2 = 0, mxt-1
-        do it1 = 0, mxt-1
+      do it2 = 0, MxT-1
+        do it1 = 0, MxT-1
           tau1 = dble(it1)*Beta/dble(MxT)
           tau2 = dble(it2)*Beta/dble(MxT)
           ibin = get_bin_Gam(it1, it2)
 
-          do dx = 0, L(1)-1
-            do dy = 0, L(2)-1
-              do ityp = 1, NTypeGam/2
-                typ = 2*(ityp-1)+1
+          do dr = 0, Vol-1
+            do ityp = 1, NTypeGam/2
+              typ = 2*(ityp-1)+1
 
-                if(IsBasis2D(ibin)) then
-                  do ibasis = 1, NBasisGam
-                    cgam = GamBasis(iorder,ityp,dx,dy,ibin,ibasis)* weight_basis_Gam( &
-                      & CoefGam(0:BasisOrderGam,0:BasisOrderGam,ibasis,ibin), tau1, tau2)
-                    Gam(typ,dx,dy,it1,it2) = Gam(typ,dx,dy,it1,it2) + cgam*normal
-                  enddo
-                else 
-                  do ibasis = 1, NBasis
-                    cgam = GamBasis(iorder, ityp, dx, dy, ibin, ibasis)* weight_basis( &
-                      & CoefGam(0:BasisOrder,0,ibasis,ibin), tau1)
-                    Gam(typ,dx,dy,it1,it2) = Gam(typ,dx,dy,it1,it2) + cgam*normal
-                  enddo
-                endif
-              enddo
+              if(IsBasis2D(ibin)) then
+                do ibasis = 1, NBasisGam
+                  cgam = GamBasis(iorder,ityp,dr,ibin,ibasis)* weight_basis_Gam( &
+                    & CoefGam(0:BasisOrderGam,0:BasisOrderGam,ibasis,ibin), tau1, tau2)
+                  Gam(typ,dr,it1,it2) = Gam(typ,dr,it1,it2) + cgam*normal
+                enddo
+              else 
+                do ibasis = 1, NBasis
+                  cgam = GamBasis(iorder, ityp, dr, ibin, ibasis)* weight_basis( &
+                    & CoefGam(0:BasisOrder,0,ibasis,ibin), tau1)
+                  Gam(typ,dr,it1,it2) = Gam(typ,dr,it1,it2) + cgam*normal
+                enddo
+              endif
             enddo
           enddo
         enddo
@@ -483,8 +373,8 @@ SUBROUTINE Gam_mc2matrix_mc
     endif
   enddo looporder
 
-  Gam(2,:,:,:,:) = Gam(1,:,:,:,:)
-  Gam(4,:,:,:,:) = Gam(3,:,:,:,:)
-  Gam(6,:,:,:,:) = Gam(5,:,:,:,:)
+  Gam(2,:,:,:) = Gam(1,:,:,:)
+  Gam(4,:,:,:) = Gam(3,:,:,:)
+  Gam(6,:,:,:) = Gam(5,:,:,:)
 
 END SUBROUTINE Gam_mc2matrix_mc
