83cf1c91a50faf4d171b787337b21a091d4bbd3b
Yuan Huang <huangy22@gmail.com>
2014-06-23 10:44:48 -0400

    delete the debug code, plot the 1st order stag chi

--- plots/3D_Heisenberg/first_order_gamma.py
+++ plots/3D_Heisenberg/first_order_gamma.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 import numpy as np
--- project/run_loop.py
+++ project/run_loop.py
@@ -6,7 +6,7 @@ import subprocess
 import time
 import logging
 
-INTERVAL = 5000
+INTERVAL = 1000
 EXEC = "./gamma3.exe"
 
 def run_loop(infile):
