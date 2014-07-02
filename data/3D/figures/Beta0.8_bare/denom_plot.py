9c47d0f4a986c3bba16ecd8dcccdc72fdb9ebc6a
Yuan Huang <huangy22@gmail.com>
2014-07-02 13:52:38 -0400

    to test the imaginary part
    Former-commit-id: 4fb36706c9b4a7b5ecdcda8c93f55715fb4ca2d3

--- data/3D/figures/Beta0.8_bare/denom_plot.py
+++ data/3D/figures/Beta0.8_bare/denom_plot.py
@@ -3,22 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import read_data
 
-Beta = 0.50
+Beta = 0.80
 N = 64
 tau = np.arange(0, Beta, Beta/N)
 
 Quans=["Denom"]
 
 Files=[]
-Files.append(read_data.read_array("../../data/3D/bare_L4_0.50_1_quantities.dat", Quans))
-Files.append(read_data.read_array("../../data/3D/bare_L4_0.50_2_quantities.dat", Quans))
-Files.append(read_data.read_array("../../data/3D/bare_L4_0.50_3_quantities.dat", Quans))
+Files.append(read_data.read_array("./0.80_quantities.dat", Quans))
+#Files.append(read_data.read_array("./0.80_d_quantities.dat", Quans))
 
 
 fig = plt.figure()
 ax = plt.subplot(111)
 
-L = 4
+L = 8
 stag = (L**2+L+1)*(L/2)
 
 
@@ -31,6 +30,6 @@ ax.legend()
 plt.xlabel("omega")
 plt.ylabel("Denom(pi, pi)")
 
-plt.savefig("Beta0.5_L4_denominator.pdf")
+plt.savefig("Beta0.8_L4_denominator.pdf")
 #plt.savefig("denominator_L8.pdf")
 plt.show()
