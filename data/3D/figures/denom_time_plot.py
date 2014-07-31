d8409daa607ba563aa0a389d193e957af237996a
Yuan Huang <huangy22@gmail.com>
2014-07-31 11:15:23 -0400

    plot and rename daily.logfile
    Former-commit-id: b42fc13162e1717169e73d452ae43c781c7e20ed

--- data/3D/figures/denom_time_plot.py
+++ data/3D/figures/denom_time_plot.py
@@ -6,18 +6,18 @@ fig = plt.figure()
 ax = plt.subplot(111)
 
 for Order in range(3, 6):
-    data = np.loadtxt("bare_L8_0.50_"+str(Order)+"_denom.dat")
+    data = np.loadtxt("bare_L8_0.80_"+str(Order)+"_denom.dat")
 
     real=[]
     for i in range(0, len(data)-1):
         real.append(data[i][0])
 
-    ax.plot(range(0, len(real)), real, marker='o', label="L=8, Beta=0.50, Order="+str(Order))
+    ax.plot(range(0, len(real)), real, marker='o', label="L=8, Beta=(0.65,0.05,0.80), Order="+str(Order))
 
 ax.legend()
 
 plt.xlabel("time")
 plt.ylabel("Denom(pi, pi, pi)")
 
-plt.savefig("Beta0.5_L8_denom_time.pdf")
+plt.savefig("Beta0.8_L8_denom_time.pdf")
 plt.show()
